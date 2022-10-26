cac52c83042db70c4b26eeb98bc6ce1f37a53982
Grzegorz <grzegorzkaw@gmail.com>
2022-10-27 01:21:59 +0200

    fixing mobile design/layout

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,10 @@ import { getPosts } from '../services';
 
 export default function Home({ posts }) {
   return (
-    <div className="w-3/4 mx-auto px-10 mb-8">
+    <div className="lg:w-3/4 mx-auto px-10 mb-8">
       <FeaturedPosts />
       <div className="flex justify-center">
-        <div className="w-3/4 lg:col-span-8 col-span-1">
+        <div className="lg:w-3/4 lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
             <PostCard key={index} post={post.node} />
           ))}
